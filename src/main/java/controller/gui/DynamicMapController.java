6b0d0f8af8e6c52b95bfc261880fd838373912ae
Gabrielle MacInnes <gabrielle.macinnes@gmail.com>
2021-10-24 00:55:16 -0400

    restoring old code

---
+++ src/main/java/controller/gui/DynamicMapController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package controller.gui;
+
+/**
+ * @author gabri
+ *
+ */
+public class DynamicMapController {
+
+	/**
+	 * @param args
+	 */
+	/*
+	@Override
+	public void start(Stage newStage) throws Exception {
+		newStage.setTitle("dynamic-map");
+		
+		newStage.show();
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    */
+}
